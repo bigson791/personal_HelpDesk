d0abad1fbe839bbf6ace3faa29a438a06d9f0d61
Elfego <eapen.b@outlook.es>
2022-11-29 23:19:39 -0600

    se actualizaron opciones de nuevo ticket

---
+++ controller/categoria.php
@@ -0,0 +1,19 @@
+<?php
+    require_once("../config/conexion.php");
+    require_once("../models/categorias.php");
+    $categoria = new Categoria();
+
+    switch($_GET["op"]){
+        case "combo":
+            $datos = $categoria->get_categoria();
+            if(is_array($datos)==true && count($datos)>0){
+            //      $html = "<option>Selecciona una opción</option>";
+                foreach($datos as $row){
+                    $html.= "<option value='".$row['cat_id']."'>".$row['cat_nom']."</option>";
+                }
+                echo $html;
+            }
+            break;
+    }
+
+?>
